cfc8901df9af54cb3926a1bd5a0f3c814063912e
SnappyCarp <126493503+SnappyCarp@users.noreply.github.com>
2023-05-18 08:41:18 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from yt_dlp import YoutubeDL
+import requests
+from tkinter import Tk, Label, Button, Text
+
+
+
+ydl_opts = {}
+def download(url):
+    try:
+      with YoutubeDL(ydl_opts) as ydl: ydl.download([url])
+    except: print('Download Error')
+
+def button_start():
+  value = txt.get(1.0, "end-1c")
+  try: r = requests.get(value)
+  except: pass
+  else:
+    if "Video unavailable" in r.text: print('Invalid Url')
+    else: download(value)
+
+def main():
+  global txt
+  root = Tk()
+  root.title("YouTube Downloader")
+  root.geometry('700x700')
+  lab = Label(root, text='Enter Video Url Here')
+  btn = Button(root, command=button_start, text='Download')
+  txt = Text(root, height = 5, width = 30)
+  lab.pack()
+  txt.pack()
+  btn.pack()
+  root.mainloop()
+
+
+main()
